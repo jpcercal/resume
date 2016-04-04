4068908512599e9d938c614a9088233fdf4c0e02
Cekurte Web Server <sistemas@cekurte.com>
2016-04-03 22:43:55 -0300

    Fixed PSR-2

--- bootstrap.php
+++ bootstrap.php
@@ -10,7 +10,7 @@ define('OUTPUT_PATH', ROOT_PATH . DS . 'output');
 
 define('APP_RESOURCES_PATH', realpath(APP_PATH . DS . 'Resources'));
 
-define('APP_RESOURCES_TWIG_PATH',   realpath(APP_RESOURCES_PATH . DS . 'template'));
+define('APP_RESOURCES_TWIG_PATH', realpath(APP_RESOURCES_PATH . DS . 'template'));
 define('APP_RESOURCES_RESUME_PATH', realpath(APP_RESOURCES_PATH . DS . 'resume'));
-define('APP_RESOURCES_I18N_PATH',   realpath(APP_RESOURCES_PATH . DS . 'i18n'));
-define('APP_RESOURCES_HELP_PATH',   realpath(APP_RESOURCES_PATH . DS . 'help'));
+define('APP_RESOURCES_I18N_PATH', realpath(APP_RESOURCES_PATH . DS . 'i18n'));
+define('APP_RESOURCES_HELP_PATH', realpath(APP_RESOURCES_PATH . DS . 'help'));
