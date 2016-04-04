4dec247006edf7d1b25cc9e140762c0d03dd31b8
Cekurte Web Server <sistemas@cekurte.com>
2016-04-03 22:39:25 -0300

    Added a bootstrap file.

---
+++ bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+
+define('ROOT_PATH', realpath(__DIR__));
+
+define('APP_PATH', realpath(ROOT_PATH . DS . 'src'));
+define('VENDOR_PATH', realpath(ROOT_PATH . DS . 'vendor'));
+define('OUTPUT_PATH', ROOT_PATH . DS . 'output');
+
+define('APP_RESOURCES_PATH', realpath(APP_PATH . DS . 'Resources'));
+
+define('APP_RESOURCES_TWIG_PATH',   realpath(APP_RESOURCES_PATH . DS . 'template'));
+define('APP_RESOURCES_RESUME_PATH', realpath(APP_RESOURCES_PATH . DS . 'resume'));
+define('APP_RESOURCES_I18N_PATH',   realpath(APP_RESOURCES_PATH . DS . 'i18n'));
+define('APP_RESOURCES_HELP_PATH',   realpath(APP_RESOURCES_PATH . DS . 'help'));
