dbe3ab3d2d821faae0a39132543b97880c58e2b9
Cekurte Web Server <sistemas@cekurte.com>
2016-04-03 22:39:56 -0300

    Added Exception classes.

---
+++ src/Exception/ApplicationException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Cekurte\Resume\Exception;
+
+class ApplicationException extends \Exception
+{
+
+}
---
+++ src/Exception/FileNotExistsException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Cekurte\Resume\Exception;
+
+use Cekurte\Resume\Exception\ApplicationException;
+
+class FileNotExistsException extends ApplicationException
+{
+
+}
