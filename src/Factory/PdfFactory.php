8b125d7f451ec4dfc0a3659f833591458cbfbe68
Cekurte Web Server <sistemas@cekurte.com>
2016-05-05 08:24:46 -0300

    Added the factories to Twig and Pdf.

---
+++ src/Factory/PdfFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Cekurte\Resume\Factory;
+
+use Cekurte\Environment\Environment;
+use Cekurte\Resume\Contract\FactoryInterface;
+use Knp\Snappy\Pdf;
+
+class PdfFactory implements FactoryInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public static function create()
+    {
+        $pdf = new Pdf(Environment::get('WKHTMLTOPDF_BIN'));
+
+        $pdf->setOption(
+            'margin-top',
+            Environment::get('WKHTMLTOPDF_OPTION_MARGIN_TOP')
+        );
+
+        $pdf->setOption(
+            'margin-right',
+            Environment::get('WKHTMLTOPDF_OPTION_MARGIN_RIGHT')
+        );
+
+        $pdf->setOption(
+            'margin-bottom',
+            Environment::get('WKHTMLTOPDF_OPTION_MARGIN_BOTTOM')
+        );
+
+        $pdf->setOption(
+            'margin-left',
+            Environment::get('WKHTMLTOPDF_OPTION_MARGIN_LEFT')
+        );
+
+        return $pdf;
+    }
+}
