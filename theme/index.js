66a20e4314e5a2bca55e28a4fd4aae50ecac8056
Elisete Cercal <elisetecercal@gmail.com>
2024-10-28 18:57:40 +0000

    add qa info to my cv

--- theme/index.js
+++ theme/index.js
@@ -1,56 +1,74 @@
-const Handlebars = require('handlebars')
-const path = require('path')
-const sass = require('sass')
-const fs = require('fs')
+const Handlebars = require("handlebars");
+const path = require("path");
+const sass = require("sass");
+const fs = require("fs");
 
-exports.render = ({ meta, basics, skills, work }) => {
-	Handlebars.registerHelper('style', () => new Handlebars.SafeString(
-		sass
-			.compile(path.join(__dirname, 'style.scss'))
-			.css
-	))
+exports.render = ({ meta, basics, skills, certificates, languages, work }) => {
+  Handlebars.registerHelper(
+    "style",
+    () =>
+      new Handlebars.SafeString(
+        sass.compile(path.join(__dirname, "style.scss")).css
+      )
+  );
 
-	Handlebars.registerHelper('compileSkills', (skills) => new Handlebars.SafeString(
-		skills.map((skill) => {
-			if (!skill.meta.display) {
-				return;
-			}
-		
-			let skillCssClasses = [];
-		
-			skillCssClasses.push(`skill`);
-			skillCssClasses.push(`skill-${skill.name.toLowerCase().replace(/ /g, '-')}`);
-			skillCssClasses.push(`skill-weight-${skill.level}`);
-		
-			if (skill.meta.highlighted) {
-				skillCssClasses.push(`skill-highlighted`);	
-			}
-		
-			return JSON.stringify({
-				'text': skill.name, 
-				'weight': skill.level,
-				'html': {
-					'class': skillCssClasses.join(' ')
-				}
-			});
-		})
-		.filter(Boolean)
-		.join(',')
-	))
+  Handlebars.registerHelper(
+    "compileSkills",
+    (skills) =>
+      new Handlebars.SafeString(
+        skills
+          .map((skill) => {
+            if (!skill.meta.display) {
+              return;
+            }
 
-	Handlebars.registerHelper('compilePhoneNumber', (telephoneNumber) => new Handlebars
-		.SafeString(telephoneNumber
-			.replace(/ /g, '')
-			.replace(/-/g, '')
-			.replace(/\(/g, '')
-			.replace(/\)/g, '')
-		))
+            let skillCssClasses = [];
 
-	Handlebars.registerHelper('compileWorkPeriod', (startDate, endDate) => endDate 
-		? `${new Date(startDate).getFullYear()} - ${new Date(endDate).getFullYear()}` 
-		: `${new Date(startDate).getFullYear()} - Present`
-	)
+            skillCssClasses.push(`skill`);
+            skillCssClasses.push(
+              `skill-${skill.name.toLowerCase().replace(/ /g, "-")}`
+            );
+            skillCssClasses.push(`skill-weight-${skill.level}`);
 
-	const template = Handlebars.compile(fs.readFileSync(path.join(__dirname, 'template.hbs'), 'utf8'))
-	return template({ meta, basics, skills, work })
-}
+            if (skill.meta.highlighted) {
+              skillCssClasses.push(`skill-highlighted`);
+            }
+
+            return JSON.stringify({
+              text: skill.name,
+              weight: skill.level,
+              html: {
+                class: skillCssClasses.join(" "),
+              },
+            });
+          })
+          .filter(Boolean)
+          .join(",")
+      )
+  );
+
+  Handlebars.registerHelper(
+    "compilePhoneNumber",
+    (telephoneNumber) =>
+      new Handlebars.SafeString(
+        telephoneNumber
+          .replace(/ /g, "")
+          .replace(/-/g, "")
+          .replace(/\(/g, "")
+          .replace(/\)/g, "")
+      )
+  );
+
+  Handlebars.registerHelper("compileWorkPeriod", (startDate, endDate) =>
+    endDate
+      ? `${new Date(startDate).getFullYear()} - ${new Date(
+          endDate
+        ).getFullYear()}`
+      : `${new Date(startDate).getFullYear()} - Present`
+  );
+
+  const template = Handlebars.compile(
+    fs.readFileSync(path.join(__dirname, "template.hbs"), "utf8")
+  );
+  return template({ meta, basics, skills, certificates, languages, work });
+};
